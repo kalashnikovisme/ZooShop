f61d31bf36600c3cdd0389043c39d6ae9ddcc213
kalashnikovisme <kalashnikovisme@gmail.com>
2013-04-26 04:44:13 +0400

    add ajax

--- www/js/basket.js
+++ www/js/basket.js
@@ -1,3 +1,28 @@
-function push_button(id) {
+function push_button(id) { 
+    if (document.getElementById("basket_" + id).style.background == "red") {
+        document.getElementById("basket_" + id).style.background = "white";
+        return;
+    }
     document.getElementById("basket_" + id).style.background = "red";
+}
+
+function confirm() {
+    var table = document.getElementById("table");
+    var count = table.rows.length - 1;
+    var arr = new Array();
+    for (var i = 1; i <= count; i++) {
+        if (document.getElementById("basket_" + i).style.background == "red") {
+            arr += i;
+        }
+    }
+    $.ajax({
+		url:    'confirm.php',
+		type:   'POST',
+		data:   'ids='+arr,
+		success: function(response) {
+			if (response == "1") {
+				alert("Заявка оформлена");
+			}
+		}
+	});
 }
--- www/php/products.php
+++ www/php/products.php
@@ -2,9 +2,11 @@
 	<head>
 		<title>РўРѕРІР°СЂС‹ РґР»СЏ РїРёС‚РѕРјС†РµРІ</title>
 		<script type="text/javascript" src="../js/basket.js"></script>
+		<script type="text/javascript" src="../js/jquery-1.4.1.js"></script>
+		<script type="text/javascript" src="../js/jquery.min.js"></script>
 	</head>
 	<body>
-		<table>
+		<table id="table">
 			<tr>
 				<th>Название</th>
 				<th>Цена</th>
@@ -14,14 +16,17 @@
 			<?php
 				$db = mysql_connect('localhost', 'root', '');
 				$select = mysql_select_db('zoo_shop', $db);
-				mysql_set_charset( 'utf8' );
+				mysql_set_charset('utf8');
 				$result = mysql_query("SELECT * FROM products");
 				if ($result) {
 					while ($data = mysql_fetch_array($result)) {
-						echo '<tr><th>'.$data["title"].'</th><th>'.$data["price"].'</th><th>'.$data["date"].'</th><th><button id = "basket_'.$data["id"].'" onclick = "push_button('.$data["id"].');">В корзину</button></th></tr>';
+						echo '<tr><th>' . $data["title"] . '</th><th>' . $data["price"] . '</th><th>' . $data["date"] . '</th><th><button style = "background: white" id = "basket_' . $data["id"] . '" onclick = "push_button(' . $data["id"] . ');">В корзину</button></th></tr>';
 					}
 				}
 			?>
 		</table>
+		<button onclick="confirm()">Подтвердить</button>
+		<div id ="div_add">
+		</div>
 	</body>
 </html>
